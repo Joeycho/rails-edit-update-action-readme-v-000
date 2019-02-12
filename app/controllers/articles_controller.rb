0b587e5f61f81df108a03d6e8ab2214e6b42bce3
Joey Cho <cjy12182000@naver.com>
2019-02-12 09:54:59 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,8 +26,8 @@ class ArticlesController < ApplicationController
   end
  
   def update
-    @article = Article.find(params[:id])
-    @article.update(title: params[:title], description: params[:description])
+    @article = Article.find(params[:article][:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
     binding.pry
     redirect_to article_path(@article)
   end
